ebc226c7694ab92d8b64b8117a40b930e28ff22c
Dainer Cortés <95858653+DainerCortesR@users.noreply.github.com>
2022-03-26 20:10:17 -0500

    Add files via upload

---
+++ javascript/mensaje.js
@@ -0,0 +1,11 @@
+function alertaenvio(){
+    swal('Felicidades', 'Dinero enviado correctamente', 'success')
+}
+
+function alertarecepcion(){
+    swal('Felicidades', 'Dinero retirado correctamente', 'success')
+}
+
+function alertabuscar(){
+    swal('Error', 'Usuario no encontrado', 'error')
+}
